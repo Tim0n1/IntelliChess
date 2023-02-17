ca5c7160271bf1ee97bc9a5b18a01550f192e4c2
timon <timonaki@abv.bg>
2023-02-17 11:53:23 +0200

    A

--- engine/engine.py
+++ engine/engine.py
@@ -1,4 +1,2 @@
 from stockfish import Stockfish
-engine = Stockfish(path='/intelliChess/stockfish_15.1_win_x64_avx2/stockfish-windows-2022-x86-64-avx2.exe')
-engine.set_fen_position('r1bqkbnr/pppp1ppp/2n5/4p3/2B1P3/5N2/PPPP1PPP/RNBQK2R')
-print(engine.get_board_visual())
+engine = Stockfish(path='C:\\Users\\timon\Desktop\dada\intelliChess\stockfish_15.1_win_x64_avx2\\stockfish-windows-2022-x86-64-avx2.exe')
